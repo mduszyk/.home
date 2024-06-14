9874d989c1646ebf1f773b050c70ba67f4a8b891
Michal <michal.duszyk@protonmail.com>
2024-06-14 16:50:45 +0200

    improving ipython title

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -8,7 +8,7 @@ c.TerminalInteractiveShell.term_title = True
 def generate_title():
     version = sys.version.split()[0]
     current_dir = os.getcwd()
-    return f"ipython{version} {current_dir}"
+    return f"ipython {version} {current_dir}"
 
 c.TerminalInteractiveShell.term_title_format = generate_title()
 
